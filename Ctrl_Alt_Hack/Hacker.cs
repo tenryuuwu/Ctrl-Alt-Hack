f49d5f42839b038a42654bd48dc186ec274c5131
tenryuuwu <nanamimadness@gmail.com>
2018-11-21 17:53:30 +0800

    added Hacker and Player class

---
+++ Ctrl_Alt_Hack/Hacker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Ctrl_Alt_Hack
+{
+    public class Hacker
+    {
+        String[] skills;
+
+        public Hacker(int numSkills)
+        {
+            skills = new String[numSkills];
+        }
+    }
+}
---
+++ Ctrl_Alt_Hack/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Ctrl_Alt_Hack
+{
+    public class Player
+    {
+        private String name;
+        private int hackerCred;
+        private int money;
+
+        public Player(String name, int hackerCred, int money)
+        {
+            this.name = name;
+            this.hackerCred = hackerCred;
+            this.money = money;
+        }
+
+        public String GetName(){
+            return name;
+        }
+
+        public void SetName(String name){
+            this.name = name;
+        }
+
+        public int GetHackerCred(){
+            return hackerCred;
+        }
+
+        public void SetHackerCred(int cred){
+            hackerCred = cred;
+        }
+
+        public int GetMoney(){
+            return money;
+        }
+
+        public void SetMoney(int money){
+            this.money = money;
+        }
+    }
+}
