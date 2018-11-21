fad4731e7fbdf2057918186cd343530c66d80e1a
tenryuuwu <nanamimadness@gmail.com>
2018-11-21 17:21:10 +0800

    Initial check-in of module Ctrl_Alt_Hack

---
+++ Ctrl_Alt_Hack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ctrl_Alt_Hack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
