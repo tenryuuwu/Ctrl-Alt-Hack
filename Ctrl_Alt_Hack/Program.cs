ab57c982923d0f40d0e6f2ff4bf26758d6d4add8
tenryuuwu <nanamimadness@gmail.com>
2018-11-21 17:30:19 +0800

    add penid

--- Ctrl_Alt_Hack/Program.cs
+++ Ctrl_Alt_Hack/Program.cs
@@ -7,6 +7,7 @@ namespace Ctrl_Alt_Hack
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello penis");
         }
     }
 }
